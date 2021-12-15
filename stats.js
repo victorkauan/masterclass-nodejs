69fea252887cab01a6a1889d91dbf0dc243e90e3
Victor Kauan <victorkauan.alu.lmb@gmail.com>
2021-12-14 21:22:06 -0300

    feat: logger

--- stats.js
+++ stats.js
@@ -1,4 +1,5 @@
 const os = require('os')
+const log = require('./logger')
 
 setInterval(() => {
   const { freemem, totalmem } = os
@@ -16,4 +17,6 @@ setInterval(() => {
   console.clear()
   console.log('====== PC Stats ======')
   console.table(stats)
+
+  log(`${JSON.stringify(stats)}\n`)
 }, 1000)
